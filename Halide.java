bdac114d432d1e8418105b1584590d46e3dceada
Halide <ahldkaradag@gmail.com>
2022-10-17 21:32:22 +0200

    intellij ile commit

--- Halide.java
+++ Halide.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Halide {
     public static void main(String[] args) {
         System.out.println("Halide");
+        /*
+         Kullanıcıya kaç tane fibonacci sayısı görmek istediğini sorunuz.
+         Ardından bu fibonacci sayılarını yazdıran kodu yazınız.
+Örnek: kullanıcı 6 girerse çıktı 1 1 2 3 5 8 olacaktır.
+         */
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Kac tane Fibonacci görmek istersiniz ?");
     }
 }
